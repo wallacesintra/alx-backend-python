44d966c22c26b5805bf74c57a400c75d9f7a76cf
wallacesintra <wallvxwallace962@gmail.com>
2024-08-17 23:00:51 +0300

    concurrent coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ wait_n that takes in 2 int arguments (in this order): n and max_delay.
 You will spawn wait_random n times with the specified max_delay.
 """
 
-import random
+
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
